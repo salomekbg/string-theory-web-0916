640306aa463d824548f826533ed75275ce5e3a1e
Katie Hoffman <ktahoffman@gmail.com>
2014-12-12 13:40:06 -0500

    add 3 line version

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -5,3 +5,11 @@ def sort_by_appearance(chars, text)
   sorted = counts.to_a.sort_by{ |a| a[1]}.reverse
   sorted.collect {|ar| ar[0]}.join
 end
+
+# 3 line version: 
+#
+# def sort_by_appearance(chars, text)
+#   count = chars.split("").each_with_object({}) { |c, h| h[c] = 0 }
+#   text.split("").each { |c| count[c] += 1 if count.keys.include?(c) }
+#   count.to_a.sort_by { |a| a[1] }.collect {|a| a.first }.reverse.join
+# end
