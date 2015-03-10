942e28001ef66fc0550d1d7043b7fc207e9d8a98
Katie Hoffman <ktahoffman@gmail.com>
2015-03-10 14:47:56 -0400

    clean up solution

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,14 +1,5 @@
-def sort_by_appearance(chars, text)
-  count = chars.split("").each_with_object({}) { |c, h| h[c] = 0 }
-  text.split("").each { |c| count[c] += 1 if count.keys.include?(c) }
-  count.to_a.sort_by { |a| a[1] }.collect {|a| a.first }.reverse.join
+def sort_by_appearance(string, text)
+  string.chars.sort_by do |c| 
+    text.scan(c).count
+  end.reverse.join
 end
-
-# short version using count and scan:
-
-# def sort_by_appearance(chars, text)                                             
-#   result = chars.split('').map do |c, h|                                        
-#     [c, text.scan(c).count]                                                     
-#   end                                                                          
-#   result.sort_by{|arr| -arr.last}.map(&:first).join                                                                                                                                  
-# end
